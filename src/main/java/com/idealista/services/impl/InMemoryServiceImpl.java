621f6e819b124c269d2d594281cdf89fc2440d63
Rubén <rubenlg94@gmail.com>
2021-04-01 19:57:13 +0200

    refactor: cambiada la forma de llamarse de unos métodos a otros en calculateScore

--- src/main/java/com/idealista/services/impl/InMemoryServiceImpl.java
+++ src/main/java/com/idealista/services/impl/InMemoryServiceImpl.java
@@ -11,99 +11,104 @@ public class InMemoryServiceImpl implements InMemoryService {
     @Override
     public int calculateScore(AdVO ad, List<PictureVO> pictures) {
         int score = 0;
-        score = checkAdPictures(pictures, score);
-        score = checkAdDescription(ad, score);
-        score = checkIfAdIsComplete(ad, pictures, score);
+        score += checkAdPictures(pictures);
+        score += checkAdDescription(ad);
+        score += checkIfAdIsComplete(ad, pictures);
 
         score = Math.max(score, 0);
         score = Math.min(score, 100);
         return score;
     }
 
-    public int checkAdPictures(List<PictureVO> pictures, int currentScore) {
+    public int checkAdPictures(List<PictureVO> pictures) {
+        int toIncrease = 0;
         if (pictures != null && !pictures.isEmpty()) {
             for (PictureVO picture : pictures) {
                 if (picture.getQuality().equals("HD")) {
-                    currentScore += 20;
+                    toIncrease += 20;
                 } else {
-                    currentScore += 10;
+                    toIncrease += 10;
                 }
             }
         } else {
-            currentScore -= 10;
+            toIncrease -= 10;
         }
-        return currentScore;
+        return toIncrease;
     }
 
-    public int checkAdDescription(AdVO ad, int currentScore) {
+    public int checkAdDescription(AdVO ad) {
+        int toIncrease = 0;
         if (ad.getDescription() != null && !ad.getDescription().isEmpty()) {
-            currentScore += 5;
-            currentScore = checkAdDescriptionLength(ad, currentScore);
-            currentScore = checkAdDescriptionAwardedWords(ad, currentScore);
+            toIncrease += 5;
+            toIncrease += checkAdDescriptionLength(ad);
+            toIncrease += checkAdDescriptionAwardedWords(ad);
         }
-        return currentScore;
+        return toIncrease;
     }
 
-    public int checkAdDescriptionLength(AdVO ad, int currentScore) {
+    public int checkAdDescriptionLength(AdVO ad) {
+        int toIncrease = 0;
         if (ad.getTypology().equals("FLAT") || ad.getTypology().equals("CHALET")) {
             String adDescription = ad.getDescription();
             int adDescriptionLength = adDescription.split(" ").length;
             if (adDescriptionLength >= 20 && adDescriptionLength <= 49) {
-                currentScore += 10;
+                toIncrease += 10;
             }
             if (adDescriptionLength >= 50) {
                 if (ad.getTypology().equals("FLAT")) {
-                    currentScore += 30;
+                    toIncrease += 30;
                 } else if (ad.getTypology().equals("CHALET")) {
-                    currentScore += 20;
+                    toIncrease += 20;
                 }
             }
         }
-        return currentScore;
+        return toIncrease;
     }
 
-    private int checkAdDescriptionAwardedWords(AdVO ad, int currentScore) {
+    private int checkAdDescriptionAwardedWords(AdVO ad) {
+        int toIncrease = 0;
         String lowerCaseDescription = ad.getDescription().toLowerCase();
         if (lowerCaseDescription.contains("luminoso")) {
-            currentScore += 5;
+            toIncrease += 5;
         }
         if (lowerCaseDescription.contains("nuevo")) {
-            currentScore += 5;
+            toIncrease += 5;
         }
         if (lowerCaseDescription.contains("céntrico")) {
-            currentScore += 5;
+            toIncrease += 5;
         }
         if (lowerCaseDescription.contains("reformado")) {
-            currentScore += 5;
+            toIncrease += 5;
         }
         if (lowerCaseDescription.contains("ático")) {
-            currentScore += 5;
+            toIncrease += 5;
         }
-        return currentScore;
+        return toIncrease;
     }
 
-    private int checkIfAdIsComplete(AdVO ad, List<PictureVO> pictures, int currentScore) {
+    private int checkIfAdIsComplete(AdVO ad, List<PictureVO> pictures) {
+        int toIncrease = 0;
         if (pictures != null && !pictures.isEmpty()) {
             switch (ad.getTypology()) {
                 case "CHALET":
                     if (ad.getDescription() != null && !ad.getDescription().isEmpty()
                             && ad.getHouseSize() != null && ad.getHouseSize() != 0
                             && ad.getGardenSize() != null && ad.getGardenSize() != 0) {
-                        currentScore += 40;
+                        toIncrease += 40;
                     }
                     break;
                 case "FLAT":
                     if (ad.getDescription() != null && !ad.getDescription().isEmpty()
                             && ad.getHouseSize() != null && ad.getHouseSize() != 0) {
-                        currentScore += 40;
+                        toIncrease += 40;
                     }
                     break;
                 case "GARAGE":
-                    currentScore += 40;
+                    toIncrease += 40;
                     break;
             }
         }
-        return currentScore;
+        return toIncrease;
     }
 
 
