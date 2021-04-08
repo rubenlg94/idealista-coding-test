267a5dacb6adfa3e286de23dc59ee79910f4ea49
Rubén <rubenlg94@gmail.com>
2021-04-08 21:54:49 +0200

    feat: añadidas constantes

---
+++ src/main/java/com/idealista/constants/Values.java
@@ -0,0 +1,11 @@
+package com.idealista.constants;
+
+public final class Values {
+
+    public static class AdsValues {
+        private AdsValues(){}
+
+        public static final int MINIMUM_SCORE = 40;
+    }
+
+}
