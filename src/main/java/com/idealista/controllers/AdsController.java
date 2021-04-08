25be8d947a4bfe4c50dd2eeda96d6091625c4a58
Rubén <rubenlg94@gmail.com>
2021-04-08 22:36:08 +0200

    refactor: métodos renombrados

--- src/main/java/com/idealista/controllers/AdsController.java
+++ src/main/java/com/idealista/controllers/AdsController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.idealista.entities.PublicAd;
 import com.idealista.entities.QualityAd;
+import com.idealista.services.InMemoryService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,18 +13,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/ads")
 public class AdsController {
 
+    private final InMemoryService inMemoryService;
+
+    public AdsController(InMemoryService inMemoryService) {
+        this.inMemoryService = inMemoryService;
+    }
+
     @GetMapping("/quality")
     public ResponseEntity<List<QualityAd>> qualityListing() {
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.OK).body(inMemoryService.qualityListing());
     }
 
     @GetMapping("/public")
     public ResponseEntity<List<PublicAd>> publicListing() {
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.OK).body(inMemoryService.publicListing());
     }
 
     @PatchMapping("/calculate-score")
     public ResponseEntity<Void> calculateScore() {
-        return ResponseEntity.notFound().build();
+        inMemoryService.scoreAds();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
